644927be9c5812eccefefb177deca12a75b926c7
Dune0 <dune_megv@hotmail.com>
2018-05-15 18:17:49 -0300

    Memotest

--- Mariela/Modulo4-js/Memotest/js/index.js
+++ Mariela/Modulo4-js/Memotest/js/index.js
@@ -1,44 +1,21 @@
 
-let carta = $("#carta");
-let cartas = [
-    {
-        name: "mulderuno",
-        img: "img/mulder_uno.jpg",
-        id: 1,
-    },
-    {
-        name: "mulderdos",
-        img: "img/mulder_dos.jpg",
-        id: 2,
-    },
-    {
-        name: "muldertres",
-        img: "img/mulder_tres.jpg",
-        id: 3,
-    },
-    {
-        name: "scullyuno",
-        img: "img/scully_uno.jpg",
-        id: 4,
-    },
-    {
-        name: "scullydos",
-        img: "img/scully_dos.jpg",
-        id: 5,
-    },
-    {
-        name: "scullytres",
-        img: "img/scully_tres.jpg",
-        id: 6,
-    },
-];// termina el Array
+var carta = document.getElementsByClassName('carta');
 
-for (var i = 0; i < cartas.length; i++){
-    cartas[i].addEventListener("click", displayCard());
- };
+var cartasArray = [
+{ name: 'mulderuno',         img: 'mulder_uno.jpg' },
+{ name: 'mulderdos',         img: 'mulder_dos.jpg' },
+{ name: 'muldertres',        img: 'mulder_tres.jpg' },
+{ name: 'scullyuno',         img: 'scully_uno.jpg' },
+{ name: 'scullydos',         img: 'scully_dos.jpg' },
+{ name: 'scullytres',        img: 'scully_tres.jpg' },
+]// termina el Array
 
- function displayCard(){
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-    this.classList.toggle("disabled");
- }
+$(".carta").on("click", function(event){ //al clickear una ficha
+	let position = $(this).data("position");//trae el data-position de donde se clickeó
+	let carta = cartas.find(function(tile) { //encuentra el objeto ficha correspondiente
+		return tile.position == position;
+	})
+	if (carta.status !== "disabled") { //si la ficha no está desactivada (porque ya encontré el par)
+		carta.flipTile();//la da vuelta
+	}
+});
