b7a1a33eb5853f71e4a210576fe2fd81d8d0c79c
Dune0 <dune_megv@hotmail.com>
2018-05-16 19:35:21 -0300

    Memopain

--- Mariela/Modulo4-js/Memotest/js/index.js
+++ Mariela/Modulo4-js/Memotest/js/index.js
@@ -1,21 +1,70 @@
+/*var jugador = $('#nombrejugador')
 
-var carta = document.getElementsByClassName('carta');
+function validar(){
+  if (jugador ==''){
+  		alert('Ingrese un nombre');
+		return false;
+		} else return true; 
+     
+    console.log(jugador);
+}
+*/
 
-var cartasArray = [
-{ name: 'mulderuno',         img: 'mulder_uno.jpg' },
-{ name: 'mulderdos',         img: 'mulder_dos.jpg' },
-{ name: 'muldertres',        img: 'mulder_tres.jpg' },
-{ name: 'scullyuno',         img: 'scully_uno.jpg' },
-{ name: 'scullydos',         img: 'scully_dos.jpg' },
-{ name: 'scullytres',        img: 'scully_tres.jpg' },
-]// termina el Array
+//$('.carta').on('click', function(){})
 
-$(".carta").on("click", function(event){ //al clickear una ficha
-	let position = $(this).data("position");//trae el data-position de donde se clickeó
-	let carta = cartas.find(function(tile) { //encuentra el objeto ficha correspondiente
-		return tile.position == position;
-	})
-	if (carta.status !== "disabled") { //si la ficha no está desactivada (porque ya encontré el par)
-		carta.flipTile();//la da vuelta
-	}
-});
+var tiles =
+["img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", "img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
+"img/scully_uno.jpg",  "img/scully_dos.jpg", "img/scully_tres.jpg", "img/scully_uno.jpg",  "img/scully_dos.jpg","img/scully_tres.jpg"];
+
+var carta = $('.carta');
+var primeraJugada = true;
+//var segundaJugada = true;
+
+
+function revelarCarta(){
+    //primera carta
+	if (primeraJugada == true){
+		firstTile = $(this);
+		primeraJugada= false;
+
+	$(this).attr("src","img/carta_oculta.png");
+	cartadescubierta= $(this).attr("src","img/carta_oculta.png");
+	console.log(firstTile);
+
+	}else{
+		actualJugada = $(this)
+	};
+	primeraJugada = true;
+
+/*segunda carta
+	if (segundaJugada == true){
+		segundaaCarta = $(this);
+		segundaJugadaa= false;
+
+	$(this).children().data("id");
+	segundaCartaDescubierta= $(this).children().data("id");
+	console.log(segundaCartaDescubierta);
+
+    }else{
+		otraJugada = $(this)
+	};
+	segundaJugada = true;
+*/
+}
+
+ // fin revelar carta
+
+
+function CrearPares(){
+}
+
+function MezclarCartas(){
+
+}
+
+//validar(jugador);
+revelarCarta();
+//$('#empezar').on('click', validar);
+$('.carta').on('click', revelarCarta);
+
+
