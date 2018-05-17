289c4c8d0db830b41f9ebac2aac20c919543cf55
Dune0 <dune_megv@hotmail.com>
2018-05-17 12:15:33 -0300

    Memory Game

--- Mariela/Modulo4-js/Memotest/js/index.js
+++ Mariela/Modulo4-js/Memotest/js/index.js
@@ -1,70 +1,84 @@
-/*var jugador = $('#nombrejugador')
+/*function cartel() {
+    var alerta;
+    var jugador = prompt("Quien juega?");
+    if (jugador == null || jugador == "") {
+        alerta = "Paraaalaa laaakaa";
+    } else {
+        alerta = "¡A disfrutar del juego xicaaa! O no...";
+    }
+ }
 
-function validar(){
-  if (jugador ==''){
-  		alert('Ingrese un nombre');
-		return false;
-		} else return true; 
-     
-    console.log(jugador);
-}
+cartel()
 */
-
-//$('.carta').on('click', function(){})
-
-var tiles =
-["img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", "img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
-"img/scully_uno.jpg",  "img/scully_dos.jpg", "img/scully_tres.jpg", "img/scully_uno.jpg",  "img/scully_dos.jpg","img/scully_tres.jpg"];
+                                                                                    /*ME MUESTRA EL DATA ID DE LA IMAGEN DE LA CARTA OCULTA*/
 
 var carta = $('.carta');
 var primeraJugada = true;
-//var segundaJugada = true;
-
+var srccarta = ($(this).attr('src') === "img/carta_oculta.png")
 
 function revelarCarta(){
-    //primera carta
-	if (primeraJugada == true){
-		firstTile = $(this);
-		primeraJugada= false;
+		if (primeraJugada == true){
+			primeraCarta = $(this);
+			primeraJugada= false;
 
-	$(this).attr("src","img/carta_oculta.png");
-	cartadescubierta= $(this).attr("src","img/carta_oculta.png");
-	console.log(firstTile);
+		$(this).data("id");
+		cartaescondida= $(this).data("id");
+		console.log(cartaescondida);
 
-	}else{
-		actualJugada = $(this)
-	};
-	primeraJugada = true;
+		}else{
+			actualJugada = $(this)
+		};
+		primeraJugada = true;
+		
+		$(this).attr('src');
+		srccarta= $(this).attr('src');
+		console.log(srccarta);
 
-/*segunda carta
-	if (segundaJugada == true){
-		segundaaCarta = $(this);
-		segundaJugadaa= false;
+}// aca finalizala function revelarCarta
 
-	$(this).children().data("id");
-	segundaCartaDescubierta= $(this).children().data("id");
-	console.log(segundaCartaDescubierta);
-
-    }else{
-		otraJugada = $(this)
-	};
-	segundaJugada = true;
-*/
-}
-
- // fin revelar carta
-
-
-function CrearPares(){
-}
-
-function MezclarCartas(){
-
-}
-
-//validar(jugador);
 revelarCarta();
-//$('#empezar').on('click', validar);
+                                                                                    /*CONSIGO UNA IMAGEN RANDOM*/
+var array = ["img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
+              "img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
+              "img/scully_uno.jpg",  "img/scully_dos.jpg", "img/scully_tres.jpg", 
+              "img/scully_uno.jpg",  "img/scully_dos.jpg","img/scully_tres.jpg"];
+
+function shuffle() {
+		var i=array.length;
+		while(i--){
+			var j=Math.floor( Math.random() * (i+1) );
+			var tmp=array[i];
+			array[i]=array[j];
+			array[j]=tmp;
+			var azar = tmp;
+		}	console.log(azar);
+} // aca finaliza la funcion shuffle
+
+shuffle();
+                                                                                    /*CAMBIO LA SRC DE LA CARTA OCULTA POR UNA RANDOM DEL ARRAY*/
+function cambiarCarta(){
+
+}
+cambiarCarta();
+                                                                                    /**/
 $('.carta').on('click', revelarCarta);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
