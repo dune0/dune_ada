aa9d2729a4588bb37d411826a9e98fa674812749
Dune0 <dune_megv@hotmail.com>
2018-05-17 12:54:56 -0300

    Memory Game, se da vuelta una imagen

--- Mariela/Modulo4-js/Memotest/js/index.js
+++ Mariela/Modulo4-js/Memotest/js/index.js
@@ -10,6 +10,28 @@
 
 cartel()
 */
+
+var azar;
+                                                                                    /*CONSIGO UNA IMAGEN RANDOM*/
+var array = ["img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
+              "img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
+              "img/scully_uno.jpg",  "img/scully_dos.jpg", "img/scully_tres.jpg", 
+              "img/scully_uno.jpg",  "img/scully_dos.jpg","img/scully_tres.jpg"];
+
+function shuffle() {
+		var i=array.length;
+		while(i--){
+			var j=Math.floor( Math.random() * (i+1) );
+			var tmp=array[i];
+			array[i]=array[j];
+			array[j]=tmp;
+			azar = tmp;
+		}	console.log(azar);
+} // aca finaliza la funcion shuffle
+
+shuffle();
+
+
                                                                                     /*ME MUESTRA EL DATA ID DE LA IMAGEN DE LA CARTA OCULTA*/
 
 var carta = $('.carta');
@@ -17,6 +39,7 @@ var primeraJugada = true;
 var srccarta = ($(this).attr('src') === "img/carta_oculta.png")
 
 function revelarCarta(){
+
 		if (primeraJugada == true){
 			primeraCarta = $(this);
 			primeraJugada= false;
@@ -31,35 +54,16 @@ function revelarCarta(){
 		primeraJugada = true;
 		
 		$(this).attr('src');
-		srccarta= $(this).attr('src');
+		srccarta= $(this).attr('src', azar);
 		console.log(srccarta);
 
 }// aca finalizala function revelarCarta
 
 revelarCarta();
-                                                                                    /*CONSIGO UNA IMAGEN RANDOM*/
-var array = ["img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
-              "img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
-              "img/scully_uno.jpg",  "img/scully_dos.jpg", "img/scully_tres.jpg", 
-              "img/scully_uno.jpg",  "img/scully_dos.jpg","img/scully_tres.jpg"];
 
-function shuffle() {
-		var i=array.length;
-		while(i--){
-			var j=Math.floor( Math.random() * (i+1) );
-			var tmp=array[i];
-			array[i]=array[j];
-			array[j]=tmp;
-			var azar = tmp;
-		}	console.log(azar);
-} // aca finaliza la funcion shuffle
+                                                                                    /**/
 
-shuffle();
-                                                                                    /*CAMBIO LA SRC DE LA CARTA OCULTA POR UNA RANDOM DEL ARRAY*/
-function cambiarCarta(){
 
-}
-cambiarCarta();
                                                                                     /**/
 $('.carta').on('click', revelarCarta);
 
