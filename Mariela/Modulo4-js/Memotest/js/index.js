b0a269aa7e97950c3cdbcbc7a3fc551650e2525f
Dune0 <dune_megv@hotmail.com>
2018-05-21 17:56:01 -0300

    Memotest version final

--- Mariela/Modulo4-js/Memotest/js/index.js
+++ Mariela/Modulo4-js/Memotest/js/index.js
@@ -1,62 +1,145 @@
-/*function cartel() {
-    var alerta;
-    var jugador = prompt("Quien juega?");
-    if (jugador == null || jugador == "") {
-        alerta = "Paraaalaa laaakaa";
-    } else {
-        alerta = "¡A disfrutar del juego xicaaa! O no...";
-    }
- }
-cartel()
-*/
+var cartasArray = [{
+    'name': 'mulder1',
+    'img': 'img/believe1.jpg'
+  }, {
+    'name': 'mulder2',
+    'img': 'img/believe2.jpg'
+  }, {
+    'name': 'mulder3',
+    'img': 'img/believe3.jpg'
+  }, {
+    'name': 'scully1',
+    'img': 'img/believe4.jpg'
+  }, {
+    'name': 'scully2',
+    'img': 'img/believe5.jpg'
+  }, {
+    'name': 'scully3',
+    'img': 'img/believe6.jpg'
+  }];
+  
+  //variables globales
+  var primerClick = '';
+  var segundoClick = '';
+  var jugadas= 0;
+  var cont = 0;
+  var previousTarget = null;
+  var delay = 800;
+  
+  //se crea la tabla de imagenes dinamicamente
+  var juego = document.getElementById('juego');
+  var tabla = document.createElement('section');
+  tabla.setAttribute('class', 'tabla');
+  juego.appendChild(tabla);
 
-var azar;
-/*CONSIGO UNA IMAGEN RANDOM*/
-var array = ["img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
-              "img/mulder_uno.jpg", "img/mulder_dos.jpg", "img/mulder_tres.jpg", 
-              "img/scully_uno.jpg",  "img/scully_dos.jpg", "img/scully_tres.jpg", 
-              "img/scully_uno.jpg",  "img/scully_dos.jpg","img/scully_tres.jpg"];
+    //cartel de bienvenida al empezar cada partida
+function cartelito() {
+  var alerta;
+  var jugador = prompt("Escribí tu nombre para comenzar a jugar:");
+    if(jugador == null || jugador == "") {
+        alerta = "Aca iba tu nombre. Me desilucionastes, negri";
+    }else{
+        alerta = "¡Ziiiiiiiiiii, " + jugador + "!";
+      }
+  document.getElementById("nombrejugador").innerHTML = alerta;
+}
+    cartelito();
 
-function shuffle() {
-		var i=array.length;
-		while(i--){
-			var j=Math.floor( Math.random() * (i+1) );
-			var tmp=array[i];
-			array[i]=array[j];
-			array[j]=tmp;
-			azar = tmp;
-		}	console.log(azar);
-} // aca finaliza la funcion shuffle
+function Memotest(){
+    var tablaJuego = cartasArray.concat(cartasArray).sort(function () {
+      return 0.5 - Math.random();
+    });
+    
+    tablaJuego.forEach(function (item) {
+      var name = item.name,
+          img = item.img;
+    
+    
+      var carta = document.createElement('div');
+      carta.classList.add('carta');
+      carta.dataset.name = name;
+    
+      var front = document.createElement('div');
+      front.classList.add('front');
+    
+      var back = document.createElement('div');
+      back.classList.add('back');
+      back.style.backgroundImage = 'url(' + img + ')';
+    
+      tabla.appendChild(carta);
+      carta.appendChild(front);
+      carta.appendChild(back);
+    });
+      
+//cuando hay un match entre dos cartas se ven las estrellas
+var match = function match() {
+    var selected = document.querySelectorAll('.selected');
+    selected.forEach(function (carta) {
+      carta.classList.add('match');
+    });
+  };
+  
+  var resetGuesses = function resetGuesses() {
+    primerClick = '';
+    segundoClick = '';
+    cont = 0;
+    previousTarget = null;
+  
+    var selected = document.querySelectorAll('.selected');
+    selected.forEach(function (carta) {
+      carta.classList.remove('selected');
+    });
+  };
+  
+  tabla.addEventListener('click', function (event) {
+  
+    var clicked = event.target;
+    
+        if (clicked.nodeName === 'SECTION' || clicked.parentNode.classList.contains('marcada') || clicked.parentNode.classList.contains('match')) {
+        return;
+        }
+        if (cont < 2) {
+            cont++;
+            if (cont === 1) {
+              primerClick = clicked.parentNode.dataset.name;
+              console.log(primerClick);
+              clicked.parentNode.classList.add('selected');
+            } else {
+              segundoClick = clicked.parentNode.dataset.name;
+              console.log(segundoClick);
+              clicked.parentNode.classList.add('selected');
+            }
+    
+            if (primerClick && segundoClick) {
+                if (primerClick === segundoClick) {
+                  setTimeout(match, delay);
+                }
+                setTimeout(resetGuesses, delay);
+              }
+            if (cont==2){
+                jugadas++
+                console.log("All moves" + jugadas);
+                var allMoves = document.getElementById('jugadas');
+                allMoves.innerHTML = 'Movimientos: ' + (jugadas);
+            }
+        }
 
-shuffle();
+            if (primerClick===segundoClick){
+              var coincidencias = document.getElementsByClassName('match');
 
-/*ME MUESTRA EL DATA ID DE LA IMAGEN DE LA CARTA OCULTA*/
+              if (coincidencias.length == 10) {
+                  setTimeout (function(){
+                      alert("Aliens are proud of you"); }, 1000);
+                  setTimeout (function() {location.reload(); }, 1500); 
+              }
+            }
 
-var carta = $('.carta');
-var primeraJugada = true;
-var srccarta = ($(this).attr('src') === "img/carta_oculta.png")
+            if (jugadas > 23 ){
+            setTimeout (function(){ alert("I want to believe you are not a loser, try again"); }, 1000)
+            setTimeout (function() {location.reload(); }, 1500);
+}
+    });
+        
+}
 
-function revelarCarta1(){
-
-		if (primeraJugada == true){
-			primeraCarta = $(this);
-			primeraJugada= false;
-
-		$(this).data("id");
-		cartaescondida= $(this).data("id");
-		console.log(cartaescondida);
-
-		}else{
-			actualJugada = $(this)
-		};
-		primeraJugada = true;
-		
-		$(this).attr('src');
-		srccarta= $(this).attr('src', azar);
-		console.log(srccarta);
-
-}// aca finalizala function revelarCarta
-
-revelarCarta();
-
-$('.carta').on('click', revelarCarta1);
+Memotest();
