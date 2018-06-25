8bf40ec2dd064be34689222d2c39ebd79a44cacc
Dune0 <dune_megv@hotmail.com>
2018-06-24 23:19:14 -0300

    weather-app

--- Mariela/Modulo5-node/weather-app/server.js
+++ Mariela/Modulo5-node/weather-app/server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
   request(url, function (err, response, body) {
     if(err){
@@ -25,7 +25,7 @@ app.post('/', function (req, res) {
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        let weatherText = `Hacen ${weather.main.temp} °C en ${weather.name}`;
         res.render('index', {weather: weatherText, error: null});
       }
     }
@@ -35,4 +35,3 @@ app.post('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-
